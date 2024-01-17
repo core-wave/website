68e6afff54469103afca2ed81381dcae63d674c8
David Oost <david@newblack.io>
2024-01-17 11:39:57 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App flex flex-col gap-6 min-h-screen w-full bg-slate-950 items-center justify-center">
-      <img src={logo} className="max-w-60" />
+      <img src={logo} className="max-w-60" alt="logo" />
       <p className="text-slate-200 text-xl">Coming soon</p>
     </div>
   );
